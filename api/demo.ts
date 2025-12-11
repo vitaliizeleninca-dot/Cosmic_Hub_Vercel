207405638c294894b47d40280de07829481eeb89
Alpha Ross <vitalii.zelenin.ca@gmail.com>
2025-12-11 19:05:04 +0200

    Update demo.ts

--- api/demo.ts
+++ api/demo.ts
@@ -1,5 +1,9 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
+export const config = {
+  runtime: "nodejs20.x"
+};
+
 export default function handler(req: VercelRequest, res: VercelResponse) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
