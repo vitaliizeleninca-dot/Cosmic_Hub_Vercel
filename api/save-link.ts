8a25737a35454b614b3da9147b920fb2aa0553be
Alpha Ross <vitalii.zelenin.ca@gmail.com>
2025-12-11 19:06:55 +0200

    Update save-link.ts

--- api/save-link.ts
+++ api/save-link.ts
@@ -1,5 +1,8 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
+export const config = {
+  runtime: "nodejs20.x"
+};
 interface LinksData {
   links: Array<{ url: string; date: string }>;
 }
