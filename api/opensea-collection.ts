6160d28cab2224f04ae9de1f75dc3623ce982c60
Alpha Ross <vitalii.zelenin.ca@gmail.com>
2025-12-11 19:06:28 +0200

    Set runtime to Node.js 20.x for OpenSea collection API

--- api/opensea-collection.ts
+++ api/opensea-collection.ts
@@ -1,5 +1,9 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
+export const config = {
+  runtime: "nodejs20.x"
+};
+
 export default async function handler(
   req: VercelRequest,
   res: VercelResponse
